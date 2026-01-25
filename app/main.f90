4326e08729aff771555f5aa740cd55b6f8f5945b
Jacob Williams <jacobwilliams@users.noreply.github.com>
2026-01-25 08:51:27 -0600

    initial commit

---
+++ app/main.f90
@@ -0,0 +1,8 @@
+program main
+    use test_module
+
+    implicit none
+
+    call run_test()
+
+end program main
---
+++ src/test.f90
@@ -0,0 +1,11 @@
+module test_module
+
+    implicit none 
+
+    contains
+
+    subroutine run_test()
+        print*, 'hello world!'
+    end subroutine run_test
+
+end module test_module
